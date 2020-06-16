c3b02c8cd04cf096d4e24cfb8345d1856d4484ee
Pooya Shams kolahi <pooya.shams.k@gmail.com>
2020-06-16 20:58:18 +0430

    Added a lot of things, like updating board

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+"use strict";
+
 const BLACK = "#000000";
 const RED = "#FF0000";
+const WHITE = "#FFFFFF";
 const WINW = 600;
 const WINH = 600;
 const numberOfCellsX = 20;
@@ -8,13 +11,22 @@ const cellW = WINW / numberOfCellsX;
 const cellH = WINH / numberOfCellsY;
 
 const defaultColor = BLACK;
+const BGCOLOR = WHITE;
 const defaultPosX = 0;
 const defaultPosY = 0;
 const lineColor = BLACK;
 const filledCellColor = BLACK;
+const ctx = document.getElementById("main-board").getContext("2d");
+
+function make_board(x = numberOfCellsX, y = numberOfCellsY) {
+  return Array(y)
+    .fill()
+    .map(() => Array(x).fill(false));
+}
+
+let board = make_board();
 
 function drawLine(
-  ctx,
   color = defaultColor,
   x1 = 0,
   y1 = 0,
@@ -33,17 +45,18 @@ function drawLine(
   return true;
 }
 
-function drawBoard(ctx) {
-  if (WINW % numberOfCellsX != 0 || WINH % numberOfCellsY) {
-    throw "cells should fit in the width and height";
-  }
+function emptyBoard() {
+  ctx.clearRect(0, 0, WINW, WINH);
+}
+
+function drawEmptyBoard() {
   // drawing vertical lines
   for (let x = cellW; x < WINW; x += cellW) {
-    drawLine(ctx, lineColor, x, 0, x, WINH);
+    drawLine(lineColor, x, 0, x, WINH);
   }
   // drawing horizontal lines
   for (let y = cellH; y < WINH; y += cellH) {
-    drawLine(ctx, lineColor, 0, y, WINW, y);
+    drawLine(lineColor, 0, y, WINW, y);
   }
   // the last two lines are slightly paler than other lines
   // but if i draw another line after them, they will be colored
@@ -52,20 +65,64 @@ function drawBoard(ctx) {
   // however if anyone knows how to stop it please tell me.
 }
 
-function drawRect(ctx, color = defaultColor, x = 0, y = 0, w = 0, h = 0) {
+function drawRect(color = defaultColor, x = 0, y = 0, w = 0, h = 0) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, w, h);
   ctx.fillStyle = defaultColor;
 }
 
-function fillCell(ctx, x, y) {
+function fillCell(x, y) {
   const x0 = x * cellW;
   const y0 = y * cellH;
-  drawRect(ctx, filledCellColor, x0, y0, cellW, cellH);
+  drawRect(filledCellColor, x0, y0, cellW, cellH);
+}
+
+function fillBoard() {
+  for (let x = 0; x < numberOfCellsX; x++)
+    for (let y = 0; y < numberOfCellsY; y++) {
+      if (board[y][x]) {
+        fillCell(x, y);
+      }
+    }
+}
+
+function drawBoard() {
+  emptyBoard();
+  drawEmptyBoard();
+  fillBoard();
+}
+
+function getState(curState, n) {
+  if (curState && (n == 2 || n == 3)) return true;
+  if (!curState && n == 3) return true;
+  return false;
+}
+
+function updateBoard() {
+  let newBoard = make_board();
+  for (let x = 0; x < numberOfCellsX; x++)
+    for (let y = 0; y < numberOfCellsY; y++) {
+      let n = -board[y][x]; // number of on neighbours
+      for (let i = -1; i <= 1; i++)
+        for (let j = -1; j <= 1; j++) {
+          const a = x + i;
+          const b = y + j;
+          if (a >= 0 && a < numberOfCellsX && b >= 0 && b < numberOfCellsY)
+            n += board[b][a];
+        }
+      newBoard[y][x] = getState(board[y][x], n);
+    }
+  for (let x = 0; x < numberOfCellsX; x++)
+    for (let y = 0; y < numberOfCellsY; y++) board[y][x] = newBoard[y][x];
+}
+
+function mainloop() {
+  drawBoard();
+  updateBoard();
 }
 
 function main() {
-  const ctx = document.getElementById("main-board").getContext("2d");
-  drawBoard(ctx);
+  setInterval(mainloop, 300);
 }
+
 main();
