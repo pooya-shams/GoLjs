9ed5918025b446af6ecb7751c9f6e520a6a492c7
Pooya Shams kolahi <pooya.shams.k@gmail.com>
2020-06-16 12:39:09 +0430

    Initial commit, added files

---
+++ script.js
@@ -0,0 +1,11 @@
+const BLACK = "#000000";
+const RED = "#FF0000";
+
+const defaultColor = BLACK;
+const defaultPosX = 0;
+const defaultPosY = 0;
+
+function main() {
+  const ctx = document.getElementById("main-board").getContext("2d");
+}
+main();
