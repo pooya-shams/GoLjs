66086139fe5cffc10d0d4676dd8f192c313b028f
Pooya Shams kolahi <pooya.shams.k@gmail.com>
2020-06-16 12:39:41 +0430

    Added drawLine function to use later

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const defaultColor = BLACK;
 const defaultPosX = 0;
 const defaultPosY = 0;
 
+function drawLine(ctx, color, x1, y1, x2, y2) {
+  ctx.fillStyle = color;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.moveTo(defaultPosX, defaultPosY);
+  return true;
+}
+
 function main() {
   const ctx = document.getElementById("main-board").getContext("2d");
 }
